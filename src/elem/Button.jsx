a671e401c01265e5ff3c6abf5083143438830188
YoungKwak <y.kwak@outlook.com>
2021-07-16 06:52:58 -0400

    생성: 뷰 작업 위한 기본 버튼/인풋 엘리먼트, 전역 스타일 색상값 외
    - 뷰 작업 위한 기본 버튼/인풋 엘리먼트 생성(수정예정)
    - 글로벌 스타일 색상 추가

--- src/elem/Button.jsx
+++ src/elem/Button.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-	return <div></div>;
+const Button = ({ children, ...rest }) => {
+	return (
+		<BtnPrimary onClick={rest._onClick} {...rest}>
+			{children}
+		</BtnPrimary>
+	);
 };
+
+Button.defaultProps = {
+	height: '40px;',
+	backgroundColor: 'var(--primary-red);',
+	color: 'var(--primary-black);',
+};
+
+const BtnPrimary = styled.button`
+	padding: 14px 4px;
+	border: none;
+	border-radius: 56px;
+	font-weight: 900;
+	font-size: 1.8rem;
+	background-color: var(--primary-red);
+	color: var(--primary-white);
+	/* background-color: ${(props) => (props.bgColor)}; */
+	cursor: pointer;
+`;
 
 export default Button;
