4f623c732e4eb264fac40afa59a3a25342b760c4
with-key <rem9238@kakao.com>
2021-07-17 23:44:48 +0900

    추가 : 유저 로그인 인증 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useEffect } from 'react';
 import Router from './components/Router';
 import GlobalStyles from './components/GlobalStyles';
+import { userAcions } from './modules/user';
+import { useDispatch } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(userAcions.__setLogin());
+	}, []);
+
 	return (
 		<>
 			<GlobalStyles />
