0a95b0eec94cfcc336a208767def1f4adbe7486e
with-key <rem9238@kakao.com>
2021-07-17 15:12:49 +0900

    S3 이미지 업로더 구현

---
+++ src/components/ImgUploader.js
@@ -0,0 +1,47 @@
+import AWS from 'aws-sdk';
+
+const ImgUploader = () => {
+	const handleFileInput = (e) => {
+		const file = e.target.files[0];
+		const upload = new AWS.S3.ManagedUpload({
+			params: {
+				Bucket: 'mypin',
+				Key: file.name,
+				Body: file,
+			},
+		});
+
+		const promise = upload.promise();
+
+		promise.then(
+			function (data) {
+				alert('이미지 업로드에 성공했습니다.');
+				console.log(data.Location);
+			},
+			function (err) {
+				return alert('오류가 발생했습니다: ', err.message);
+			},
+		);
+	};
+
+	AWS.config.update({
+		region: 'ap-northeast-2',
+		credentials: new AWS.CognitoIdentityCredentials({
+			IdentityPoolId: 'ap-northeast-2:c5f7eeba-60e4-4887-b943-cca4271abd60',
+		}),
+	});
+
+	return (
+		<>
+			<input
+				type='file'
+				id='upload'
+				className='image-upload'
+				onChange={handleFileInput}
+			/>
+			<label htmlFor='upload' className='image-upload-wrapper' />
+		</>
+	);
+};
+
+export default ImgUploader;
