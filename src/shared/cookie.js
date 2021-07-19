fac1811d9be251d830486dca056836f266638ce1
with-key <rem9238@kakao.com>
2021-07-19 09:20:57 +0900

    생성 : 쿠키 로직 생성

--- src/shared/cookie.js
+++ src/shared/cookie.js
@@ -1,4 +1,4 @@
-const setCookie = (name, value, exp = 5) => {
+const setCookie = (name, value, exp = 1) => {
 	let date = new Date();
 	date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
 	document.cookie = `${name}=${value}; expires=${date.toUTCString()}`;
