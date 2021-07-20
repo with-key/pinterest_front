08dc2d788add7e4616f3eca2c43701e750284adf
with-key <rem9238@kakao.com>
2021-07-20 11:17:01 +0900

    수정 : pin 모듈 수정

--- src/modules/pin.js
+++ src/modules/pin.js
@@ -4,36 +4,47 @@ import { pinApi } from '../shared/api';
 
 // action
 const GET_PINLIST = 'pin/GET_PINLIST';
+const ADD_FORM = 'pin/ADD_FORM';
+const ADD_PIN = 'pin/ADD_PIN';
 
 // initState
+
 const initState = {
 	list: [],
 	isLogin: false,
+	pin: null,
 };
 
 // action creator
-export const getPinList = createAction(GET_PINLIST, (pinList) => ({pinList}));
+// const addForm = createAction(ADD_FORM, (newForm) => ({ newForm }));
+export const getPinList = createAction(GET_PINLIST, (pinList) => ({
+	pinList,
+}));
+export const addPin = createAction(ADD_PIN, (pin) => ({ pin }));
 
 // Thunk function
-export const __getPinList =
-	(props) =>
-	async (dispatch) => {
-		try {
-			const { data } = await pinApi.getPinList();
-			console.log(data);
-			dispatch(getPinList(data));
-		} catch (e) {
-			console.log(e);
-		}
-	};
+export const __getPinList = (props) => async (dispatch) => {
+	try {
+		const { data } = await pinApi.getPinList();
+		dispatch(getPinList(data));
+	} catch (e) {
+		console.log(e);
+	}
+};
+export const __addPin = () => (dispatch) => {
+	try {
+	} catch (e) {}
+};
 
 // reducer
 const pin = handleActions(
 	{
 		[GET_PINLIST]: (state, action) => {
-			return { ...state,
-				list: action.payload.articles,// 리덕스액션을 사용하면 action 내에 있는 데이터를 무조건 'payload'로 불러옵니다.
-		}}
+			return {
+				...state,
+				list: action.payload.articles,
+			};
+		},
 	},
 
 	initState,
