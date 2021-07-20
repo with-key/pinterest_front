6d0ff37dd48762a0542abd1c661e579ce5b4c75b
with-key <rem9238@kakao.com>
2021-07-20 11:13:37 +0900

    수정 : 핀 추가 페이지 route 지정

--- src/components/Router.js
+++ src/components/Router.js
@@ -6,19 +6,15 @@ import Auth from '../shared/auth';
 import Home from '../pages/Home';
 import Signup from '../pages/Signup';
 import PinDetail from '../pages/PinDetail';
+import AddPin from '../pages/AddPin';
 
 const Router = () => {
 	return (
 		<Switch>
-			{/* 두번째 파라미터
-				null (비로그인, 로그인 모두 접속 가능)
-				true (로그인만 접속 가능)
-				false (비로그인만 접속 가능)
-				*/}
-
 			<Route path='/' component={Auth(Home, null)} exact />
-			<Route path='/signup' component={Auth(Signup, false)} exact />
+			<Route path='/main' component={Auth(Signup, false)} exact />
 			<Route path='/pin/:id' component={Auth(PinDetail, null)} exact />
+			<Route path='/pin-bulider' component={Auth(AddPin, null)} exact />
 		</Switch>
 	);
 };
