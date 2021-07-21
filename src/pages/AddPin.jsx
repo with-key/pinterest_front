787d6977d5c75219cd432d3ce683a8bc3d3e742d
with-key <rem9238@kakao.com>
2021-07-21 16:54:41 +0900

    추가 : pin 추가 페이지 추가

--- src/pages/AddPin.jsx
+++ src/pages/AddPin.jsx
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Template from '../components/Template';
 import AddForm from '../components/AddForm';
-import { Button } from '../elem';
-import { useDispatch, useSelector } from 'react-redux';
+import { Icons } from '../elem';
+import { useSelector } from 'react-redux';
 
 const AddPin = ({ history }) => {
-	const dispatch = useDispatch();
+	const previewUrl = useSelector((store) => store.image.preview);
 
 	return (
 		<Template bg='var(--primary-lightgray)' pd='28px 0'>
 			<LeftNav>
-				<Button />
+				<AddformBtn />
+				<MiniView previewUrl={previewUrl}>
+					{previewUrl ? '' : <Icons.Paint />}
+				</MiniView>
 			</LeftNav>
-			<Container>
-				<AddForm />
-			</Container>
+			<AddForm />
 		</Template>
 	);
 };
 
-const Container = styled.section`
-	background-color: #fff;
-`;
-
 const LeftNav = styled.nav`
 	position: absolute;
-	left: 0;
+	left: 10px;
 	top: 100px;
-	background-color: #fff;
-	border: 1px solid red;
-	width: 100px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	padding: 16px;
 `;
+
+const MiniView = styled.div`
+	background-image: url(${(props) => props.previewUrl});
+	background-size: cover;
+	background-position: center center;
+	width: 40px;
+	height: 60px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 4px;
+	background-color: #e2e2e2;
+`;
+
+const AddformBtn = styled.div`
+	width: 40px;
+	height: 60px;
+	border-radius: 4px;
+	background-color: #e2e2e2;
+`;
+
 export default AddPin;
